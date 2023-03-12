c1cb74ec873d4fe4a2b368fceef3c8cbf5a15e5e
SMLukwiya <sundaymorganl@gmail.com>
2023-03-12 12:13:41 +0100

    add color palette

---
+++ AlarmApp/Constants/ColorConstants.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+let backgroundColor = Color("backgroundColor")
+let darkYelloe = Color("darkYellow")
+let lightYellow = Color("lightYellow")
+let cardBackgroundColor = Color("cardBackgroundColor")
+let darkGray = Color("darkGray")
+let lightGray = Color("lightGray")
+let blue = Color("blue")
+let black = Color("black")
+
+let gradientColors: [Color] = [.indigo, blue, darkYelloe, lightYellow]
