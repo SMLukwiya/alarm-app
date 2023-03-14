80719ac2825a961daf2d9f053372233ccfdb98b4
SMLukwiya <sundaymorganl@gmail.com>
2023-03-14 10:19:21 +0100

    add select activity view

--- AlarmApp/Constants/ColorConstants.swift
+++ AlarmApp/Constants/ColorConstants.swift
@@ -8,5 +8,10 @@ let darkGray = Color("darkGray")
 let lightGray = Color("lightGray")
 let blue = Color("blue")
 let black = Color("black")
+let lightBlue = Color("lightBlue")
+let pink = Color("pink")
+let white = Color("white")
 
 let gradientColors: [Color] = [.indigo, blue, darkYellow, lightYellow]
+
+let mainColors = [blue, pink, white]
---
+++ AlarmApp/Views/AuxilliaryViews/SelectActivity.swift
@@ -0,0 +1,40 @@
+
+import SwiftUI
+
+struct SelectActivity: View {
+    @Binding var currentColorIndex: Int
+    @Binding var currentActivity: String
+    
+    var currentColor: Color {
+        mainColors[currentColorIndex]
+    }
+    
+    var nextIndex: Int {
+        (currentColorIndex + 1) % mainColors.count
+    }
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(activities, id: \.self) { activity in
+                    let selectedActivity = activity == currentActivity
+                    
+                    Image(systemName: activity)
+                        .font(selectedActivity ? .title  : .subheadline)
+                        .foregroundColor(selectedActivity ? currentColor : lightBlue)
+                        .onTapGesture {
+                            withAnimation {
+                                currentActivity = activity
+                            }
+                        }
+                }
+            }
+        }
+    }
+}
+
+struct SelectActivity_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectActivity(currentColorIndex: .constant(0), currentActivity: .constant(activities[0]))
+            .padding()
+    }
+}
