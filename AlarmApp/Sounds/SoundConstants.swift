c884f1aef0251fa9873cbca7271c1811047144b1
SMLukwiya <sundaymorganl@gmail.com>
2023-03-15 18:28:03 +0100

    add sounds constants

--- AlarmApp/Sounds/SoundConstants.swift
+++ AlarmApp/Sounds/SoundConstants.swift
@@ -2,17 +2,26 @@
 import Foundation
 
 enum Sounds: String, CaseIterable, Codable {
-case acoustic_guitar = "acoustic-guitar-1.mp3"
-case drumbit = "drumbit-1.mp3"
-case smooth_guitar = "smooth-guitar-1.mp3"
-case soft_piano = "soft-piano.mp3"
-case trap_loop = "tra-loop.mp3"
-case trap_loop_1 = "trap-loop-2.mp3"
-case witchy_cackie = "witchy-cackie.mp3"
-    
-    func formatSoundName() -> String {
-        String(describing: self)
-            .replacingOccurrences(of: "_", with: " ").capitalized
-    }
+    case acoustic_guitar = "acoustic-guitar.mp3"
+    case drumbit = "drumbit.mp3"
+    case smooth_guitar = "smooth-guitar.mp3"
+    case soft_piano = "soft-piano.mp3"
+    case trap_loop = "tra-loop.mp3"
+    case trap_loop_1 = "trap-loop-1.mp3"
+    case witchy_cackie = "witchy-cackie.mp3"
 }
 
+extension String {
+    var formatSoundName: String {
+        var result = String(describing: self)
+            .replacingOccurrences(of: "_", with: " ").capitalized
+        
+        let periodIndex = result.firstIndex(of: ".")
+        
+        if let periodIndex = periodIndex {
+            result.removeSubrange(periodIndex...)
+        }
+        
+        return result
+    }
+}
